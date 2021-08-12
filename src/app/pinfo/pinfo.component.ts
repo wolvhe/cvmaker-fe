759ff180d6b5047135ca35f395c459d4885e96b0
akilesh <akilesh@codingmart.com>
2021-08-12 12:15:41 +0530

    minor styling

---
+++ src/app/pinfo/pinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pinfo',
+  templateUrl: './pinfo.component.html',
+  styleUrls: ['./pinfo.component.css']
+})
+export class PinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
