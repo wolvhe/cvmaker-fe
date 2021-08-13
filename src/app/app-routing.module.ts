3d68a50d65a42cad1a81a43bfaa7689fd89f6547
pavi <pavithirandxd@gmail.com>
2021-08-13 14:50:16 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { PinfoComponent } from './pinfo/pinfo.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExperienceComponent } from './experience/experience.component';
+import { TesttqueryComponent } from './testtquery/testtquery.component';
 
 const routes: Routes = [
   {path: 'info', component: PinfoComponent},
-  {path: 'experience', component: ExperienceComponent}
+  {path: 'experience', component: ExperienceComponent},
+  {path: 'test', component: TesttqueryComponent}
 ];
 
 @NgModule({
