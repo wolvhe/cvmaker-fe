27efca262e862839e1f55b1e9a1aa36010f7a891
karthikeyan <karthikeyan@codingmart.com>
2021-08-14 12:05:59 +0530

    styiling changes

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+declare var $:any;
 
 @Component({
   selector: 'app-experience',
@@ -9,10 +9,33 @@ import * as $ from 'jquery';
 
 export class ExperienceComponent implements OnInit {
  
+
   constructor() { }
 
   ngOnInit(){
+    // $.fn.animatePrependTo = function(sel: any, speed: any) {
+    //   var $this = this,
+    //     newEle = $this.clone(true).prependTo(sel),
+    //     newPos = newEle.position();
+    //   newEle.hide();
+    //   $this.css('position', 'absolute').animate(newPos, speed, function() {
+    //     newEle.show();
+    //     $this.remove();
+    //   });
+    //   return newEle;
+    // };
     
+    // $('#swap1').on("click", function() {
+    //   var tmp = $('#inner1').parent();
+    //   $('#inner1').animatePrependTo($("#inner2").parent(), 1000);
+    //   $('#inner2').animatePrependTo(tmp, 1000);
+    // });
+    
+    // $('.swap2').on("click", function() {
+    //   var tmp = $('#inner4').parent();
+    //   $('#inner3').animatePrependTo($("#inner3").parent(), 1000);
+    //   $('#inner4').animatePrependTo(tmp, 1000);
+    // });
   }
   editorModules = {toolbar: [
     [
