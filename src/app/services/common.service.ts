3dfd71f44c780ec73ac5a1b9ed72f483ff31b48e
pavi <pavithirandxd@gmail.com>
2021-08-16 15:49:26 +0530

    changes

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -8,11 +8,29 @@ import { ReplaySubject } from 'rxjs';
 })
 export class CommonService {
   sharedStyleSource = new ReplaySubject<any>(1);
-  public sharedStyle$ = this.sharedStyleSource.asObservable();
+  sharedEdinStyleSource = new ReplaySubject<any>(1);
+  sharedEdinTextStyleSource = new ReplaySubject<any>(1);
+  sharedPrinStyleSource = new ReplaySubject<any>(1);
+  sharedPrinTextStyleSource = new ReplaySubject<any>(1);
 
+  
+  public sharedStyle$ = this.sharedStyleSource.asObservable();
+  public sharedEdinStyle$ = this.sharedEdinStyleSource.asObservable();
+  public sharedEdinTextStyle$ = this.sharedEdinTextStyleSource.asObservable();
+  public sharedPrinStyle$ = this.sharedPrinStyleSource.asObservable();
+  public sharedPrinTextStyle$ = this.sharedPrinTextStyleSource.asObservable();
+  
   constructor() { }
   newStyle(value: any) {
     this.sharedStyleSource.next(value);
   }
-  
+  newEdinStyle(value: any) {
+    this.sharedEdinStyleSource.next(value);
+  }
+  newEdinTextStyle(value: any) {
+    this.sharedEdinTextStyleSource.next(value);
+  }
+  newPrinTextStyle(value: any) {
+    this.sharedPrinTextStyleSource.next(value);
+  }
 }
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -18,22 +18,68 @@ export class TemplateComponent implements OnInit {
   // changeColor() {
   //   this.bgColor = 'grey';
   // }
-  onClickColor1() {
+  onClickAckuColor1() {
     this.styling.newStyle({ 'background-color': 'lightslategray' });
   }
-  onClickColor2() {
+  onClickAckuColor2() {
     this.styling.newStyle({ 'background-color': 'pink' });
   }
-  onClickColor3() {
+  onClickAckuColor3() {
     this.styling.newStyle({ 'background-color': 'rgb(250, 250, 119)' });
   }
-  onClickColor4() {
+  onClickAckuColor4() {
     this.styling.newStyle({ 'background-color': 'rgb(119, 219, 250)' });
   }
-  onClickColor5() {
+  onClickAckuColor5() {
     this.styling.newStyle({ 'background-color': 'rgb(248, 149, 142)' });
   }
-  onClickColor6() {
+  onClickAckuColor6() {
     this.styling.newStyle({ 'background-color': 'rgb(19, 161, 0)' });
   }
+  // edin
+  onClickEdinColor1() {
+    this.styling.newEdinStyle({ 'background-color': 'lightslategray' });
+    this.styling.newEdinTextStyle({ 'color': 'lightslategray' });
+  }
+  onClickEdinColor2() {
+    this.styling.newEdinStyle({ 'background-color': 'hotpink' });
+    this.styling.newEdinTextStyle({ 'color': 'hotpink' });
+  }
+  onClickEdinColor3() {
+    this.styling.newEdinStyle({ 'background-color': 'darkorange' });
+    this.styling.newEdinTextStyle({ 'color': 'darkorange' });
+  }
+  onClickEdinColor4() {
+    this.styling.newEdinStyle({ 'background-color': 'darkslateblue' });
+    this.styling.newEdinTextStyle({ 'color': 'darkslateblue' });
+  }
+  onClickEdinColor5() {
+    this.styling.newEdinStyle({ 'background-color': 'brown' });
+    this.styling.newEdinTextStyle({ 'color': 'brown' });
+  }
+  onClickEdinColor6() {
+    this.styling.newEdinStyle({ 'background-color': 'rgb(19, 161, 0)' });
+    this.styling.newEdinTextStyle({ 'color': 'darkgreen' });
+  }
+
+  //princeton
+  onClickPrinColor1() {
+    this.styling.newPrinTextStyle({ 'color': 'black', 'font-weight': 'bold' });
+  }
+  onClickPrinColor2() {
+    this.styling.newPrinTextStyle({ 'color': 'rgb(202, 52, 52)', 'font-weight': 'bold' });
+  }
+  onClickPrinColor3() {
+    this.styling.newPrinTextStyle({ 'color': 'rgb(45, 58, 133)', 'font-weight': 'bold' });
+  }
+  onClickPrinColor4() {
+    this.styling.newPrinTextStyle({ 'color': 'rgb(99, 57, 57)', 'font-weight': 'bold' });
+  }
+  onClickPrinColor5() {
+    this.styling.newPrinTextStyle({ 'color': 'rgb(68, 155, 140)', 'font-weight': 'bold' });
+  }
+  onClickPrinColor6() {
+    this.styling.newPrinTextStyle({ 'color': 'darkorange', 'font-weight': 'bold' });
+  }
+  
 }
