7d91b5b53db20a8519676a92fd8c396715825c8b
akilesh <akilesh@codingmart.com>
2021-08-18 16:20:03 +0530

    enhancement for skills courses and lang

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -19,7 +19,29 @@ export class ExperienceComponent implements OnInit {
     this.formArray = this.formArray.filter((d,i) => i!= index)
   }
 
+  filterSkillArray(index: any) {
+    console.log(index)
+    this.formSkillArray = this.formSkillArray.filter((d,i) => i!= index)
+  }
+
+  filterLanguageArray(index: any) {
+    console.log(index)
+    this.formLanguageArray = this.formLanguageArray.filter((d,i) => i!= index)
+  }
+
+  filterCourseArray(index: any) {
+    console.log(index)
+    this.formCourseArray = this.formCourseArray.filter((d,i) => i!= index)
+  }
+
+  
   formArray = [{}]
+
+  formSkillArray = [{}]
+
+  formLanguageArray = [{}]
+
+  formCourseArray = [{}]
   
   editorModules = {toolbar: [
     [
